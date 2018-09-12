4c7917aa1a0bd67bb6f29c51db326f107aaf7aae
yomi2201 <37528909+yomi2201@users.noreply.github.com>
2018-09-12 21:51:06 +0900

    end15

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ArticlesController extends Controller
+{
+    public function index() {
+        $articles = Article::all();
+
+        return view('articles.index',compact('articles'));
+    }
+
+    public function show($id) {
+        $article = Article::findOrfail($id);
+        
+        return view('articles.show',compact('article'));
+    }
+}
